30343b7deef8cb30d82e75d06dc9d9f440d3e071
minshadpc <minshadpc486@gmail.com>
2024-08-27 12:37:27 +0530

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,24 @@
-export let cart = JSON.parse(localStorage.getItem("cart"));
+export let cart;
 
-if (!cart) {
-  cart = [
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2,
-      deliveryOptionsId: "1",
-      deliveryOptionsId: "2",
-    },
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1,
-    },
-  ];
+laodFromStorage();
+
+export function laodFromStorage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
+
+  if (!cart) {
+    cart = [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionsId: "1",
+        deliveryOptionsId: "2",
+      },
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+      },
+    ];
+  }
 }
 
 function saveToStorage(params) {
@@ -53,7 +59,7 @@ export function removeFromCart(productId) {
   saveToStorage();
 }
 
- export function updateDeliveryOption(productId, deliveryOptionId) {
+export function updateDeliveryOption(productId, deliveryOptionId) {
   let mathingItem;
 
   cart.forEach((cartItem) => {
