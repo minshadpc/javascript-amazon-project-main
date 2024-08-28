6dd12d6ca06aba142f08467a947ebbdf298cf0cf
minshadpc <minshadpc486@gmail.com>
2024-08-28 10:35:31 +0530

    Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import "../data/car-oop.js";
+import "../data/cart-class.js";
 renderOrderSummary();
 renderPaymentSummary();
