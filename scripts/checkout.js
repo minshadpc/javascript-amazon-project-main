3286c1be83ecfa558e47abc77a960940e824398c
minshadpc <minshadpc486@gmail.com>
2024-09-08 20:22:06 +0530

    nothing

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,17 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import "../data/cart-class.js";
 //import '../data/backend-practice.js'
+
+async function laodPage() {
+  console.log("laod page");
+
+  laod
+
+}
+laodPage().then(() => {
+  console.log("next page");
+});
+
 import { loadProducts } from "../data/products.js";
 loadProducts(() => {
   renderOrderSummary();
